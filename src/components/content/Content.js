533df627fef4fc8a375b721d603ca25220608c26
Makhinur Ibaidilaeva <makhinuribaidilaeva@MacBook-Pro-Makhinur.local>
2024-01-29 19:17:52 +0600

    argen

--- src/components/content/Content.js
+++ src/components/content/Content.js
@@ -1,5 +1,31 @@
 import React from "react";
+import img4 from "../images/Product Image (4).png";
 
 export default function Content() {
-  return <div>Content</div>;
+  return (
+    <div>
+      <div className="container">
+        <div className="lo">
+          <div className="li">
+            <img src={img4} alt="" />
+            <h1>THE BOY, THE MOLE, THE FOX AND THE HORSE</h1>
+            <br />
+          </div>
+          <div className="pi">
+            <p>by Charlie Mackesy</p>
+            <br />
+            <br />
+            <br />
+            Lorem ipsum dolor sit amet, consectetur <br /> adipiscing elit, sed
+            do eiusmod tempor <br /> incididunt ut labore et dolore magna <br />{" "}
+            aliqua.
+            <br />
+            <br />
+            <h2>$99</h2>
+            <input type="text" name="Add to Cart " placeholder="Add to Cart " />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
